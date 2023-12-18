ee595f758e19bf1a64a15e07ce35c2877934b051
MeghanaBait <133511757+MeghanaBait@users.noreply.github.com>
2023-12-18 10:42:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 let a = prompt("num1:");
-const result = parseInt(a) * 2;
+let result = parseInt(a) * 2;
 let b = prompt("num2:");
 result = result + parseInt(b);
 alert("The final result is: "+result);
